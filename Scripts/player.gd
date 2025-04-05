1444f8ae8e48d29555565da06a9bbd44bb18dcf7
Leonardo Céspedes <lecespedes@estudiantec.cr>
2025-04-04 18:37:37 -0600

    Player Movement & StateMachine

---
+++ Scripts/player.gd
@@ -0,0 +1,47 @@
+class_name Player extends CharacterBody2D
+
+var cardinal_direction : Vector2 = Vector2.DOWN
+var direction : Vector2 = Vector2.ZERO
+
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var state_machine: PlayerStateMachine = $StateMachine
+
+func _ready() -> void:
+	state_machine.Initialize(self)
+	pass
+
+func _process(delta: float) -> void:
+	direction.x = Input.get_action_strength('right') - Input.get_action_strength('left')
+	direction.y = Input.get_action_strength('down') - Input.get_action_strength('up')
+	pass
+	
+func _physics_process(delta: float) -> void:
+	move_and_slide()
+
+func SetDirection() -> bool:
+	var new_direction : Vector2 = cardinal_direction
+	if direction == Vector2.ZERO:
+		return false
+	if direction.y == 0:
+		new_direction = Vector2.LEFT if direction.x < 0 else Vector2.RIGHT
+	elif direction.x == 0:
+		new_direction = Vector2.UP if direction.y < 0 else Vector2.DOWN
+		
+	if new_direction == cardinal_direction:
+		return false
+	cardinal_direction = new_direction
+	# Scale to affect child elements
+	animated_sprite_2d.scale.x = -1 if cardinal_direction == Vector2.LEFT else 1
+	return true
+	
+func UpdateAnimation(state : String) -> void:
+	animated_sprite_2d.play( state + "_" + AnimDirection())
+	pass	
+
+func AnimDirection() -> String:
+	if cardinal_direction == Vector2.DOWN:
+		return "down"
+	elif cardinal_direction == Vector2.UP:
+		return "up"
+	else:
+		return "side"
